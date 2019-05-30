4fe81e4a5f9bb6470dfe8cacd3b56fd30d2d8ef7
Yash Mishra <yash.mishra@raweng.com>
2019-05-30 18:19:41 +0530

    Updates

---
+++ precache-manifest.4f5b398527624ae5cba8e4d6de4c3608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91358e378b1007c4d24088dc53828f76",
+    "url": "/dice-react/index.html"
+  },
+  {
+    "revision": "049317b97975094e2e92",
+    "url": "/dice-react/static/css/main.0253e14e.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/dice-react/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "049317b97975094e2e92",
+    "url": "/dice-react/static/js/main.a7c4009f.chunk.js"
+  },
+  {
+    "revision": "20f385446d2cf665257b",
+    "url": "/dice-react/static/js/runtime~main.fa866a7b.js"
+  }
+]);
